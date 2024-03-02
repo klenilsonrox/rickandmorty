5eec11099bc3f2e6eaa715c81a5804d34ab6e4f4
klenilsonrox <klenilsoncoc@gmail.com>
2024-03-02 16:31:01 -0300

    atualizando

--- src/app/personagens/[id]/page.tsx
+++ src/app/personagens/[id]/page.tsx
@@ -12,9 +12,9 @@ const page = async ({ params }: Params) => {
   const dados = await response.json()
 
   return (
-    <div className="p-2">
+    <div className="p-2 max-w-sm mx-auto flex items-center justify-center">
       <div className="flex gap-2 mx-auto flex-wrap max-w-sm w-screen  my-4 border-2 overflow-hidden rounded-md" key={dados.id}>
-          <img src={dados.image} alt="" className="w-full"/>
+          <img src={dados.image} alt="" className="w-screen"/>
           <div className="p-2">
           <h1>Nome: <strong>{dados.name}</strong></h1>
           <p className="flex gap-2 items-center">Status: <span className={`h-3 rounded-full w-3 flex ${dados.status==="Alive" ? "bg-green-600":""} ${dados.status==="Dead" ? "bg-red-600":""} ${dados.status==="unknown" ? "bg-slate-400":""}` }></span> <strong>{dados.status}</strong></p>
